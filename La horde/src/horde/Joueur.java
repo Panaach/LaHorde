ca4a784e87205b9621e7f72373aa194f5325a56e
plaut2u <44429145+plaut2u@users.noreply.github.com>
2018-10-24 11:01:54 +0200

    Update Joueur.java

--- La horde/src/horde/Joueur.java
+++ La horde/src/horde/Joueur.java
@@ -1,5 +1,15 @@
 package horde;
 
 public class Joueur {
-
+  int pointsDeVie = 100;
+  int pointsAction = 6;
+  String nom;
+  boolean enVie = true;
+  Case caseActuelle;
+  
+  public Joueur(String n, int pdv, int pa){
+    String n = this.nom;
+    int pdv = this.pointsDeVie;
+    int pa = this.pointsAction;
+  }
 }
